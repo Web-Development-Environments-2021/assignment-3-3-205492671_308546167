1d7dfa9c0aab31bddd6682b2e408c2c4b5434412
Naor <Naors1144@gmail.com>
2021-06-14 00:56:20 +0300

    play game store

---
+++ src/pages/PlayerPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+        <b-card
+            no-body
+            style="max-width: 30rem;"
+            img-src="https://placekitten.com/380/200"
+            img-alt="Image"
+            img-top
+        >
+            <template #header>
+            <h4 class="mb-0">Player Name</h4>
+            </template>
+
+            <b-list-group flush>
+                <b-list-group-item>Full name:</b-list-group-item>
+                <b-list-group-item>Group name:</b-list-group-item>
+                <b-list-group-item>Position number:</b-list-group-item>
+                <b-list-group-item>Common name:</b-list-group-item>
+                <b-list-group-item>Nationality:</b-list-group-item>
+                <b-list-group-item>Date of birth:</b-list-group-item>
+                <b-list-group-item>Contry of birth:</b-list-group-item>
+                <b-list-group-item>Height:</b-list-group-item>
+                <b-list-group-item>Weight:</b-list-group-item>
+            </b-list-group>
+    </div>      
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -6,7 +6,85 @@ const state = {
     favorite_matches: {
         matches: [],
         fresh: false // indicates whater need to bring favorite matches from server
-    }
+    },
+    players_naor:{
+        players: [
+            {
+                "player_id": 37418793,
+                "team_id": 211,
+                "country_id": 2507,
+                "position_id": 2,
+                "common_name": "J. Gomez",
+                "display_name": "J. Gomez",
+                "fullname": "James Gomez",
+                "firstname": "James",
+                "lastname": "Gomez",
+                "nationality": "Gambia",
+                "birthdate": null,
+                "birthcountry": "Gambia",
+                "birthplace": null,
+                "height": null,
+                "weight": null,
+                "image_path": "https://cdn.sportmonks.com/images/soccer/players/9/37418793.png"
+            },
+            {
+                "player_id": 286609,
+                "team_id": 271,
+                "country_id": 275,
+                "position_id": 1,
+                "common_name": "W. Fariñez Aray",
+                "display_name": "Wuilker Faríñez",
+                "fullname": "Wuilker Fariñez Aray",
+                "firstname": "Wuilker",
+                "lastname": "Fariñez Aray",
+                "nationality": "Venezuela",
+                "birthdate": "15/02/1998",
+                "birthcountry": "Venezuela",
+                "birthplace": null,
+                "height": "175 cm",
+                "weight": "84 kg",
+                "image_path": "https://cdn.sportmonks.com/images/soccer/placeholder.png"
+            },
+            {
+                "player_id": 95481,
+                "team_id": 271,
+                "country_id": 17,
+                "position_id": 1,
+                "common_name": "J. Leca",
+                "display_name": "Jean-Louis Leca",
+                "fullname": "Jean-Louis Leca",
+                "firstname": "Jean-Louis",
+                "lastname": "Leca",
+                "nationality": "France",
+                "birthdate": "21/09/1985",
+                "birthcountry": "France",
+                "birthplace": "Bastia",
+                "height": "180 cm",
+                "weight": "75 kg",
+                "image_path": "https://cdn.sportmonks.com/images/soccer/players/25/95481.png"
+            }
+        ]
+    },
+    test_naor:{
+
+                "player_id": 37418793,
+                "team_id": 211,
+                "country_id": 2507,
+                "position_id": 2,
+                "common_name": "J. Gomez",
+                "display_name": "J. Gomez",
+                "fullname": "James Gomez",
+                "firstname": "James",
+                "lastname": "Gomez",
+                "nationality": "Gambia",
+                "birthdate": null,
+                "birthcountry": "Gambia",
+                "birthplace": null,
+                "height": null,
+                "weight": null,
+                "image_path": "https://cdn.sportmonks.com/images/soccer/players/9/37418793.png"
+            }
+        
 }
 const actions = {
     login(username, roles) {

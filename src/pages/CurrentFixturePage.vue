bc209a2f3336d63f1240b8efa8d3d9020fcd08ce
ozlia <63154309+ozlia@users.noreply.github.com>
2021-06-16 22:03:40 +0300

    fix current fixture bug

--- src/pages/CurrentFixturePage.vue
+++ src/pages/CurrentFixturePage.vue
@@ -1,7 +1,45 @@
 <template>
     <div>
-       <b-table striped hover :items="pre_matches"></b-table>
-       <b-table striped hover :items="post_matches"></b-table>
+       <b-table striped hover :items="pre_matches">
+         <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+
+      <template #cell(eventlog)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? 'Hide' : 'Show'}} Events
+        </b-button>
+      </template>
+
+      <template #row-details="row">
+        <div class="eventlog">
+          <event v-for="e in row.item.eventlog" :event="e" :key="e.min_in_game"></event>
+        </div>
+      </template>
+       </b-table>
+       <b-table striped hover :items="post_matches">
+         <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+
+      <template #cell(eventlog)="row">
+        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
+          {{ row.detailsShowing ? 'Hide' : 'Show'}} Events
+        </b-button>
+      </template>
+
+      <template #row-details="row">
+        <div class="eventlog">
+          <event v-for="e in row.item.eventlog" :event="e" :key="e.min_in_game"></event>
+        </div>
+      </template>
+       </b-table>
     </div>
 </template>
 
